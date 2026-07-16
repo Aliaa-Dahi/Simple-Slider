096cb82a26440fdb3398e98d0c4e50fb9f73d5db
Aliaa <aliaamohamaddahi@gmail.com>
2026-07-16 23:40:42 +0300

    change the image in the slider when clicking on an image

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let imgList = document.querySelectorAll(".item img"); // NodeList\
 let lightContainer = document.querySelector(".light-container");
 let btnClose = document.getElementById("close");
+let box = document.querySelector(".box");
 
 for (let i = 0; i < imgList.length; i++) {
-  imgList[i].addEventListener("click", function () {
+  imgList[i].addEventListener("click", function (e) {
+    let currentSrc = e.target.getAttribute("src");
     lightContainer.classList.remove("d-none");
+    box.style.backgroundImage = `url(${currentSrc})`;
   });
 }
 
