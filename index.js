0cf7162a7732fd8ac75e5dcb3a241f7174dc878e
Aliaa <aliaamohamaddahi@gmail.com>
2026-07-17 00:03:49 +0300

    handle prev and next buttons

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+// selecting elements
 let imgList = document.querySelectorAll(".item img"); // NodeList\
 let lightContainer = document.querySelector(".light-container");
 let btnClose = document.getElementById("close");
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
 let box = document.querySelector(".box");
+
+// variables
+let index = 0;
 
 for (let i = 0; i < imgList.length; i++) {
   imgList[i].addEventListener("click", function (e) {
+    index = i;
     let currentSrc = e.target.getAttribute("src");
     lightContainer.classList.remove("d-none");
     box.style.backgroundImage = `url(${currentSrc})`;
@@ -13,4 +20,14 @@ for (let i = 0; i < imgList.length; i++) {
 
 btnClose.addEventListener("click", function () {
   lightContainer.classList.add("d-none");
+});
+
+nextBtn.addEventListener("click", function () {
+  if (index == imgList.length - 1) index = -1;
+  box.style.backgroundImage = `url(${imgList[++index].src})`;
+});
+
+prevBtn.addEventListener("click", function () {
+  if (index == 0) index = imgList.length;
+  box.style.backgroundImage = `url(${imgList[--index].src})`;
 });
