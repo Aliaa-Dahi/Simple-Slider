e9d2e4b1c1b8c853e3c8086940e5834f6eafe63e
Aliaa <aliaamohamaddahi@gmail.com>
2026-07-16 23:21:33 +0300

    make the slider layout and design

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var myImgs = document.querySelectorAll(".sub-img img"); // NodeList
-var maniImg = document.querySelector(".main-img");
+// var myImgs = document.querySelectorAll(".sub-img img"); // NodeList
+// var maniImg = document.querySelector(".main-img");
 
-for (var i = 0; i < myImgs.length; i++) {
-  myImgs[i].addEventListener("click", function (e) {
-    var currentSrc = e.target.getAttribute("src");
-    maniImg.setAttribute("src", currentSrc);
-  });
-}
+// for (var i = 0; i < myImgs.length; i++) {
+//   myImgs[i].addEventListener("click", function (e) {
+//     var currentSrc = e.target.getAttribute("src");
+//     maniImg.setAttribute("src", currentSrc);
+//   });
+// }
